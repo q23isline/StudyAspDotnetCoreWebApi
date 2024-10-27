7c9ec4805c6065dc20bc38d2a94506df3bd374d8
q23isline <rowpure@gmail.com>
2024-10-27 14:36:05 +0900

    add: プロフィールモデル作成

--- StudyAspDotnetCoreWebApi.Server/Models/MyContext.cs
+++ StudyAspDotnetCoreWebApi.Server/Models/MyContext.cs
@@ -6,8 +6,19 @@ public class MyContext : DbContext
 {
     public MyContext(DbContextOptions<MyContext> options) : base(options) { }
 
+    public DbSet<Profile> Profiles { get; set; } = default!;
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.AddInterceptors(new TimestampInterceptor());
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Profile>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.Sex).HasDefaultValue("0").IsFixedLength();
+        });
     }
 }
---
+++ StudyAspDotnetCoreWebApi.Server/Models/Profile.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyAspDotnetCoreWebApi.Models;
+
+public partial class Profile : IRecordableTimestamp
+{
+    public Guid Id { get; set; } = Guid.Empty;
+
+    [MaxLength(50)]
+    [Display(Name = "名")]
+    public required string FirstName { get; set; }
+
+    [MaxLength(50)]
+    [Display(Name = "姓")]
+    public required string LastName { get; set; }
+
+    [MaxLength(50)]
+    [Display(Name = "名（かな）")]
+    public required string FirstNameKana { get; set; }
+
+    [MaxLength(50)]
+    [Display(Name = "姓（かな）")]
+    public required string LastNameKana { get; set; }
+
+    [MaxLength(1)]
+    [RegularExpression("0|1|2|9")]
+    [Display(Name = "性別")]
+    public required string Sex { get; set; } = "0";
+
+    [Display(Name = "誕生日")]
+    public DateOnly? BirthDay { get; set; }
+
+    [MaxLength(11)]
+    [Display(Name = "携帯電話番号")]
+    public string? CellPhoneNumber { get; set; }
+
+    [Display(Name = "メモ")]
+    public string? Remarks { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastUpdatedAt { get; set; }
+}
